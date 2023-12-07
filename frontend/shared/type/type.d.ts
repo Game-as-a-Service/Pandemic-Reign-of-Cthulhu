006cab1ccee2dc9b85fce76010aec13204cd832c
KalpasWang <wf6212@gmail.com>
2023-12-07 20:16:15 +0800

    feature: add investigator card picking ui page

--- frontend/shared/type/type.d.ts
+++ frontend/shared/type/type.d.ts
@@ -1,2 +1,16 @@
-type Investigator = 'detective' | 'doctorate' | 'driver' | 'hunter' | 'magician' | 'occultist' | 'reporter'
-type Sanity = 'sane' | 'insane'
+type Investigator =
+  | "detective"
+  | "doctorate"
+  | "driver"
+  | "hunter"
+  | "magician"
+  | "occultist"
+  | "reporter";
+type Sanity = "sane" | "insane";
+
+interface InvestigatorData {
+  name: Investigator;
+  saneImage: string;
+  insaneImage: string;
+  sanity: Sanity;
+}
